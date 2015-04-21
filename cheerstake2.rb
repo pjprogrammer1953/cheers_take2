3143a9f7e5c9bd81921b5bf57b1f656145bbcaa9
pjprogrammer1953 <pjprogrammer1953@gmail.com>
2015-04-21 13:38:03 -0500

    First upload

---
+++ cheerstake2.rb
@@ -0,0 +1,11 @@
+# many tests unit test case
+require "rubygems"
+require "bundler/setup"
+require "minitest"
+
+class 
+
+  def method_name
+  	
+  end
+end
---
+++ test_cheers_integration.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+
+class TestCheersIntegration < Minitest::Test
+
+  def test_that_the_tests_run
+    assert_equal true, false
+  end
+end
